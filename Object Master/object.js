41aa230aa9b0a24574eb39912937be5df5ecf2c1
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-12-07 12:47:09 +0200

    add

--- Object Master/object.js
+++ Object Master/object.js
@@ -1,37 +1,13 @@
-const pokémon = Object.freeze([
-        { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
-        { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
-        { "id": 9,   "name": "Blastoise",  "types": ["water"] },
-        { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
-        { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
-        { "id": 23,  "name": "Ekans",      "types": ["poison"] },
-        { "id": 24,  "name": "Arbok",      "types": ["poison"] },
-        { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
-        { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
-        { "id": 52,  "name": "Meowth",     "types": ["normal"] },
-        { "id": 63,  "name": "Abra",       "types": ["psychic"] },
-        { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
-        { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
-        { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
-        { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
-        { "id": 98,  "name": "Krabby",     "types": ["water"] },
-        { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-        { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
-        { "id": 133, "name": "Eevee",      "types": ["normal"] },
-        { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
-        { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
-        { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
-        { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
-    ]);
-
-    const pokemon = pokémon.filter( p => p.id % 3 ==0 );
-    const pokemon1 = pokémon.filter( item => item.types.includes("fire") );
-    const pokemon2 = pokémon.filter( item => item.types.length>1);
-    const pokemon3 = pokémon.map( p => p.name );
-    const pokemon4 = pokémon.filter( p => p.id >99 ).map(p => p.name );
-    const pokemon5 = pokémon.filter( p => p.types.length === 1 && p.types[0]==="poison").map(p => p.name );
-    const pokemon6 = pokémon.filter( p => p.types[1]==="flying").map(p => p.types[0] );
-    const pokemon7 = pokémon.filter( item => item.types.includes("normal") );
-
-    console.log(pokemon7.length);
+const person = { 
+    firstName: 'Bob', 
+    lastName: 'Marley', 
+    email: 'bob@marley.com', 
+    password: 'sekureP@ssw0rd9', 
+    username: 'barley', 
+    createdAt: 1543945177623
+};
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+var email = person.email;
+var firstAnimal = animals[0];
+console.log(email)
     
