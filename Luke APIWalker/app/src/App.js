83358b44aaa066bc81f151791b17844905eace01
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-12-15 16:21:05 +0200

    add

--- Luke APIWalker/app/src/App.js
+++ Luke APIWalker/app/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
+  import logo from './logo.svg';
+  import './App.css';
+  import React ,{useState, useEffect} from 'react'
+  import {Router} from '@reach/router';
+  import List from './components/List';
+  import Person from './components/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <List>
-        
-      </List>
-    </div>
-  );
-}
 
-export default App;
+  function App() {
+    return (
+      <div className="App">
+        <List/>
+      <Router>
+      <Person path="/:search/:id" />
+      </Router>
+      </div>
+    );
+  }
+
+
+  export default App;
--- Luke APIWalker/app/src/components/List.jsx
+++ Luke APIWalker/app/src/components/List.jsx
@@ -1,18 +1,27 @@
-import React ,{useState} from 'react'
+  import React ,{useState} from 'react'
+  import { navigate } from '@reach/router'
 
-const List=(props)=> {
-    const[state,setState]=useState(false);
-    const showDropdown=()=>{
-        setState(true);
-    }
-    const hideDropdown=()=>{
-        setState(false);
-    }
-  return (
-    <div>
-     
-    </div>
-  )
-}
+  const List=(props)=> {
+    const [type, setType] = useState("people");
+    const [id, setId] = useState("");
+    
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        navigate("/"+type+"/"+id)}
 
-export default List
+      return (
+        <div>
+      <label>Search for:</label>
+          <form onSubmit={handleSubmit}>
+          <select value={type} onChange={(e)=>setType(e.target.value)}>
+            <option value="people">people</option>
+            <option value="planets">planets</option>
+          </select>
+          <input type="text" onChange={(e)=>setId(e.target.value)}/>
+          <button>search</button>
+          </form>
+          
+        </div>
+      )
+    }
+  export default List;
